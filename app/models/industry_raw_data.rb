bb9291faffb10eac6cd9079b74c1ada6b051232b
mwatha <mwathabwanali@gmail.com>
2014-03-22 15:19:20 +0200

    changing import entry

--- app/models/industry_raw_data.rb
+++ app/models/industry_raw_data.rb
@@ -2,5 +2,5 @@ class IndustryRawData < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :country ,:class_name => :Country, :foreign_key => :country_id                                                        
   belongs_to :manufacturer ,:class_name => :Manufacturer, :foreign_key => :brand_name_id                                                        
-  belongs_to :salt_type ,:class_name => :SaltType, :foreign_key => :salt_type_id                                                       
+  belongs_to :salt_brand ,:class_name => :Product, :foreign_key => :salt_brand_id                                                       
 end
